aa70005f68bdf690115d6a7b448d8b69e7cdf890
Bayerlova <michaela.bayerlova@wsu.edu>
2020-09-16 13:42:39 -0700

    all files

---
+++ functions/libraries.R
@@ -0,0 +1,12 @@
+# libraries ...
+
+library(pryr);          # mem_used()
+library(lmtest);        # lmtest has lrtest
+library(MASS);          # polr
+
+install.packages("tractor.base")
+library(tractor.base);  # implode
+
+library(stringr);
+library(rvest);			# imdb web scraping
+
