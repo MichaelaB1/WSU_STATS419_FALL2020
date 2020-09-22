865f9db5cb6fbc27d7dadb73565977b3567961c5
Bayerlova <michaela.bayerlova@wsu.edu>
2020-09-22 11:50:21 -0700

    week3 files

--- functions/libraries.R
+++ functions/libraries.R
@@ -4,12 +4,12 @@ library(pryr);          # mem_used()
 library(lmtest);        # lmtest has lrtest
 library(MASS);          # polr
 
-install.packages("tractor.base")
+#install.packages("tractor.base")
 library(tractor.base);  # implode
 
 library(stringr);
 library(rvest);			# imdb web scraping
 
-install.packages("data.table")
-library(data.table);
+#install.packages("data.table")
+#library(data.table);
 
